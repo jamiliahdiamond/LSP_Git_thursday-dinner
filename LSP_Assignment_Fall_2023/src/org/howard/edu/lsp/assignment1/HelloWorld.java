38f54834aa482e9ddcb2f4eafd3bc172b5bee0ed
jamiliah <jamiliah@localhost>
2023-09-04 19:14:30 -0400

    Initial Commit

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	
+	/**
+	 *  Jamiliah Eubanks
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
